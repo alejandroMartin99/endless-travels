55041f6d6848aa1b2e1ea5ef14a691871333d37f
Alex_mi99 <alexmiglesias@gmail.com>
2025-04-23 21:45:56 +0200

    edited movile version

--- src/app/pages/destinations/japan/japan.component.ts
+++ src/app/pages/destinations/japan/japan.component.ts
@@ -1,25 +1,10 @@
-import { Component, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
-import { MatTabGroup } from '@angular/material/tabs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-japan',
   templateUrl: './japan.component.html',
   styleUrl: './japan.component.css'
 })
-export class JapanComponent implements OnInit {
-  @ViewChild(MatTabGroup) tabGroup!: MatTabGroup;
-  selectedTabIndex = 2;
-  
-  constructor(private cdr: ChangeDetectorRef) {}
+export class JapanComponent {
 
-  ngOnInit(): void {
-    // Inicialización básica
-  }
-
-  // Usa OnTabChanged para controlar el cambio de pestañas
-  onTabChanged(event: any): void {
-    // Evitar ciclos de detección innecesarios
-    this.selectedTabIndex = event.index;
-    this.cdr.detectChanges();
-  }
 }
