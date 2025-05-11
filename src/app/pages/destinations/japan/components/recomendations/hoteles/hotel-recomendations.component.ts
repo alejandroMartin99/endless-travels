5059d44c641e14864d5944ce92d1fa04ba06cfd7
Alex_mi99 <alexmiglesias@gmail.com>
2025-05-11 17:48:23 +0200

    word hotel 2

--- src/app/pages/destinations/japan/components/recomendations/hoteles/hotel-recomendations.component.ts
+++ src/app/pages/destinations/japan/components/recomendations/hoteles/hotel-recomendations.component.ts
@@ -54,6 +54,11 @@ export class HotelRecomendationsComponent  implements OnInit{
   hotel1_Url: string =`https://www.booking.com/hotel/jp/chuan-qi-kingusukaihurontodong-ji-reihoteru.es.html?label=brave_nonbrand_organic_trigger_ef205286-b3e9-46b0-863b-817cac9a3b88_0&sid=5bf532fdc7db13af6869e23c0b013a12&aid=2405612&ucfs=1&arphpl=1&checkin=2025-08-04&checkout=2025-08-11&dest_id=-233143&dest_type=city&group_adults=4&req_adults=4&no_rooms=2&group_children=0&req_children=0&hpos=1&hapos=1&sr_order=popularity&srpvid=d7cd984526800534&srepoch=1746827712&all_sr_blocks=316945805_347606635_2_2_0%2C316945805_347606635_2_2_0&highlighted_blocks=316945805_347606635_2_2_0%2C316945805_347606635_2_2_0&matching_block_id=316945805_347606635_2_2_0&sr_pri_blocks=316945805_347606635_2_2_0__11175192%2C316945805_347606635_2_2_0__11175192&from=searchresults)`
 
 
+  hotel2_Url: string =`https://www.booking.com/hotel/jp/tokyu-stay-kyoto-ryogaemachi-dori.es.html?aid=2405612&label=brave_nonbrand_organic_trigger_ef205286-b3e9-46b0-863b-817cac9a3b88_0&sid=5bf532fdc7db13af6869e23c0b013a12&checkin=2025-10-29&checkout=2025-11-05&dest_id=-235402&dest_type=city&dist=0&group_adults=2&group_children=0&hapos=1&hpos=1&no_rooms=1&req_adults=2&req_children=0&room1=A%2CA&sb_price_type=total&soh=1&sr_order=popularity&srepoch=1746977857&srpvid=3a596ddd594d04f7&type=total&ucfs=1&#no_availability_msg`
+
+
+
+
   getSafeDescription(description: string): SafeHtml {
       // Reemplaza URLs con un enlace que muestra "Link" en lugar de la URL completa
       const linkifiedDescription = description.replace(
@@ -70,6 +75,7 @@ export class HotelRecomendationsComponent  implements OnInit{
       '/assets/japan/Recomendations/Hotel/Hotel1.png',
       '/assets/japan/Recomendations/Hotel/Hotel1_2.png',
       '/assets/japan/Recomendations/Hotel/Hotel1_3.png',
+      '/assets/japan/Recomendations/Hotel/Hotel1_4.png',
  ]
 
     changeImage(delta: number): void {
