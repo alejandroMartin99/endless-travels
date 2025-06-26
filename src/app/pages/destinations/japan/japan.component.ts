6d9d58e3be04f38fbab81c84d4dcab9bdfd4ea73
Alex_mi99 <alexmiglesias@gmail.com>
2025-06-26 20:00:54 +0200

    japan main inicio

--- src/app/pages/destinations/japan/japan.component.ts
+++ src/app/pages/destinations/japan/japan.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class JapanComponent {
 
-  selectedTabIndex = 3;
+  selectedTabIndex = 0;
 
   changeTab(index: number) {
     this.selectedTabIndex = index;
